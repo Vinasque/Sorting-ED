75a00c5c1051867a2b27677824a39e4f8642a8c6
Guilherme Buss <guibuss10@gmail.com>
2024-05-21 21:03:09 -0300

    Template implementado no Selection

--- Selection/functions.cpp
+++ Selection/functions.cpp
@@ -5,150 +5,168 @@
 
 using namespace std;
 
-Node* createNode(int iPayload) 
-{
-    Node* temp = (Node*)malloc(sizeof(Node));
-    temp->iPayload = iPayload;
-    temp->ptrNext = nullptr;
-    temp->ptrPrev = nullptr;
-    return temp;
-}
 
-void insertEnd(Node** head, int iPayload) 
-{
-    Node* newNode = createNode(iPayload);
+namespace functionsSelection {
+    template <typename T>
+    Node<T>* createNode(T iPayload) 
+    {
+        Node<T>* temp = (Node<T>*)malloc(sizeof(Node<T>));
+        temp->iPayload = iPayload;
+        temp->ptrNext = nullptr;
+        temp->ptrPrev = nullptr;
+        return temp;
+    }
 
-    if (*head == nullptr) 
+    template <typename T>
+    void insertEnd(Node<T>** head, T iPayload) 
     {
-        *head = newNode;
-    } 
-    else 
-    {
-        Node* temp = *head;
-        
-        while (temp->ptrNext != nullptr) 
+        Node<T>* newNode = createNode(iPayload);
+
+        if (*head == nullptr) 
         {
-            temp = temp->ptrNext;
-        }
-        
-        temp->ptrNext = newNode;
-        newNode->ptrPrev = temp;
-    }
-}
-
-void displayList(Node* node) 
-{
-    if (node == nullptr) 
-    {
-        cout << "Lista vazia." << endl;
-        return;
-    }
-
-    cout << "Payload: ";
-    while (node != nullptr) 
-    {
-        cout << node->iPayload << " ";
-        node = node->ptrNext;
-    }
-    
-    cout << endl;
-}
-
-void swapNodes(Node* node1, Node* node2) 
-{
-    int temp = node1->iPayload;
-    node1->iPayload = node2->iPayload;
-    node2->iPayload = temp;
-}
-
-void deleteList(Node** head) {
-    if (*head == nullptr) {
-        cout << "Lista já está vazia." << endl;
-        return;
-    }
-
-    Node* current = *head;
-    Node* nextNode = nullptr;
-
-    while (current != nullptr) {
-        nextNode = current->ptrNext;
-        free(current);
-        current = nextNode;
-    }
-
-    *head = nullptr;
-}
-
-void selectionSort(Node* head) 
-{
-    if (head == nullptr || head->ptrNext == nullptr) return;
-
-    int iLength = 0;
-    Node* current = head;
-    
-    // Determina o comprimento da lista
-    while (current != nullptr) 
-    {
-        iLength++;
-        current = current->ptrNext;
-    }
-
-    current = head;
-
-    for (int iOuterLoop = 0; iOuterLoop < iLength; iOuterLoop++) 
-    {        
-        Node* temp = current->ptrNext;
-
-        for (int iInnerLoop = iOuterLoop + 1; iInnerLoop < iLength; iInnerLoop++) 
+            *head = newNode;
+        } 
+        else 
         {
-            if (current->iPayload > temp->iPayload) 
+            Node<T>* temp = *head;
+            
+            while (temp->ptrNext != nullptr) 
             {
-                swapNodes(current, temp);
+                temp = temp->ptrNext;
             }
             
-            temp = temp->ptrNext;
+            temp->ptrNext = newNode;
+            newNode->ptrPrev = temp;
+        }
+    }
+
+    template <typename T>
+    void displayList(Node<T>* node) 
+    {
+        if (node == nullptr) 
+        {
+            cout << "Lista vazia." << endl;
+            return;
         }
 
-        current = current->ptrNext;
-    }
-}
-
-void optimizedSelectionSort(Node* head) 
-{
-    if (head == nullptr || head->ptrNext == nullptr) return;
-
-    int iLength = 0;
-    Node* current = head;
-    
-    // Determina o comprimento da lista
-    while (current != nullptr) 
-    {
-        iLength++;
-        current = current->ptrNext;
-    }
-
-    current = head;
-
-    for (int iOuterLoop = 0; iOuterLoop < iLength - 1; iOuterLoop++) 
-    {
-        Node* temp = current->ptrNext;
-        Node* smallest = current->ptrNext;
-
-        for (int iInnerLoop = iOuterLoop + 1; iInnerLoop < iLength; iInnerLoop++) 
+        cout << "Payload: ";
+        while (node != nullptr) 
         {
-            if (smallest->iPayload > temp->iPayload) 
+            cout << node->iPayload << " ";
+            node = node->ptrNext;
+        }
+        
+        cout << endl;
+    }
+
+    template <typename T>
+    void swapNodes(Node<T>* node1, Node<T>* node2) 
+    {
+        T temp = node1->iPayload;
+        node1->iPayload = node2->iPayload;
+        node2->iPayload = temp;
+    }
+
+    template <typename T>
+    void deleteList(Node<T>** head) {
+        if (*head == nullptr) {
+            cout << "Lista já está vazia." << endl;
+            return;
+        }
+
+        Node<T>* current = *head;
+        Node<T>* nextNode = nullptr;
+
+        while (current != nullptr) {
+            nextNode = current->ptrNext;
+            free(current);
+            current = nextNode;
+        }
+
+        *head = nullptr;
+    }
+
+    template <typename T>
+    void selectionSort(Node<T>* head) 
+    {
+        if (head == nullptr || head->ptrNext == nullptr) return;
+
+        int iLength = 0;
+        Node<T>* current = head;
+        
+        // Determina o comprimento da lista
+        while (current != nullptr) 
+        {
+            iLength++;
+            current = current->ptrNext;
+        }
+
+        current = head;
+
+        for (int iOuterLoop = 0; iOuterLoop < iLength; iOuterLoop++) 
+        {        
+            Node<T>* temp = current->ptrNext;
+
+            for (int iInnerLoop = iOuterLoop + 1; iInnerLoop < iLength; iInnerLoop++) 
             {
-                smallest = temp;
+                if (current->iPayload > temp->iPayload) 
+                {
+                    swapNodes(current, temp);
+                }
+                
+                temp = temp->ptrNext;
             }
 
-            temp = temp->ptrNext;
+            current = current->ptrNext;
         }
-
-        if (smallest->iPayload < current->iPayload)
-        {
-            swapNodes(current, smallest);
-        }
-
-        current = current->ptrNext;
     }
+
+    template <typename T>
+    void optimizedSelectionSort(Node<T>* head) 
+    {
+        if (head == nullptr || head->ptrNext == nullptr) return;
+
+        int iLength = 0;
+        Node<T>* current = head;
+        
+        // Determina o comprimento da lista
+        while (current != nullptr) 
+        {
+            iLength++;
+            current = current->ptrNext;
+        }
+
+        current = head;
+
+        for (int iOuterLoop = 0; iOuterLoop < iLength - 1; iOuterLoop++) 
+        {
+            Node<T>* temp = current->ptrNext;
+            Node<T>* smallest = current->ptrNext;
+
+            for (int iInnerLoop = iOuterLoop + 1; iInnerLoop < iLength; iInnerLoop++) 
+            {
+                if (smallest->iPayload > temp->iPayload) 
+                {
+                    smallest = temp;
+                }
+
+                temp = temp->ptrNext;
+            }
+
+            if (smallest->iPayload < current->iPayload)
+            {
+                swapNodes(current, smallest);
+            }
+
+            current = current->ptrNext;
+        }
+    }
+
+    template Node<int>* createNode<int>(int iPayload);
+    template void insertEnd<int>(Node<int>** head, int iPayload);
+    template void displayList<int>(Node<int>* head);
+    template void swapNodes<int>(Node<int>* node1, Node<int>* node2);
+    template void deleteList<int>(Node<int>** head);
+    template void selectionSort<int>(Node<int>* head);
+    template void optimizedSelectionSort<int>(Node<int>* head);
 }
